cf40687754a6ce004a0b157095de90834072927f
Mike Wang <mikenyc1207@gmail.com>
2020-05-18 22:01:20 -0400

    update basic web server

---
+++ basic-web-server/basic-web-server/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  basic-web-server
+//
+//  Created by Zihan Wang on 5/17/20.
+//  Copyright © 2020 wanzih. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
